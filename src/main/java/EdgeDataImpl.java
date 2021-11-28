d9e95aa0aba08f4ba59afca9754cd562c5208f40
Daniel-Ros <Danimalau@live.com>
2021-11-28 21:22:03 +0200

    Initial commit

---
+++ src/main/java/EdgeDataImpl.java
@@ -0,0 +1,55 @@
+import api.EdgeData;
+
+public class EdgeDataImpl implements EdgeData {
+    private int src,dest,weight,tag;
+    private String info;
+
+    public EdgeDataImpl(int src, int dest, int weight) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    public EdgeDataImpl(int src, int dest, int weight, int tag, String info) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+        this.tag = tag;
+        this.info = info;
+    }
+
+    @Override
+    public int getSrc() {
+        return src;
+    }
+
+    @Override
+    public int getDest() {
+        return dest;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String getInfo() {
+        return info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        tag = t;
+    }
+}
