2ed23b328d30b103f17a8599814a22659f736a5d
DZ <62887535+sunscity@users.noreply.github.com>
2021-12-08 17:58:25 +0200

    add remove edit

--- src/main/java/GUI/MenuBar.java
+++ src/main/java/GUI/MenuBar.java
@@ -36,6 +36,9 @@ public class MenuBar extends JMenuBar implements ActionListener {
     JMenuItem deleteVertex;
     JMenuItem addEdge;
     JMenuItem deleteEdge;
+    JMenuItem editVertex;
+    JMenuItem editEdge;
+
 
     DirectedWeightedGraphAlgorithms ga;
 
@@ -53,6 +56,9 @@ public class MenuBar extends JMenuBar implements ActionListener {
         sep = new JSeparator();
         exit = new JMenuItem("exit");
 
+
+        editVertex = new JMenuItem("Edit");
+        editEdge = new JMenuItem("Edit");
         addVertex = new JMenuItem("Add");
         deleteVertex = new JMenuItem("Delete");
         addEdge = new JMenuItem("Add");
@@ -64,6 +70,8 @@ public class MenuBar extends JMenuBar implements ActionListener {
         isConnected = new JMenuItem("Is Connected");
         center = new JMenuItem("Center");
 
+        editEdge.addActionListener(this);
+        editVertex.addActionListener(this);
         addVertex.addActionListener(this);
         deleteVertex.addActionListener(this);
         addEdge.addActionListener(this);
@@ -92,8 +100,10 @@ public class MenuBar extends JMenuBar implements ActionListener {
 
         vertex.add(addVertex);
         vertex.add(deleteVertex);
+        vertex.add(editVertex);
         edges.add(addEdge);
         edges.add(deleteEdge);
+        edges.add(editEdge);
 
         graph.add(vertex);
         graph.add(edges);
@@ -342,6 +352,92 @@ public class MenuBar extends JMenuBar implements ActionListener {
                 int dest= Integer.parseInt(inputDest.getText());
                 if(ga.getGraph().getEdge(src,dest)!=null && ga.getGraph().getNode(src)!=null && ga.getGraph().getNode(dest)!=null){
                     ga.getGraph().removeEdge(src,dest);
+                }else{
+                    JOptionPane.showMessageDialog(null,"Wrong Input");
+                }
+            }
+            getTopLevelAncestor().repaint();
+        }else if(e.getSource()==editEdge){
+            JPanel getSrcDestWeightPanel = new JPanel(new GridLayout(5,2));
+            JCheckBox editWeightCheckBox = new JCheckBox("Keep Weight: ");
+            JTextField inputSrc = createFilteredTextField(false);
+            JTextField inputDest = createFilteredTextField(false);
+            JTextField inputSrcNew = createFilteredTextField(false);
+            JTextField inputDestNew = createFilteredTextField(false);
+            JTextField inputWeight = createFilteredTextField(true);
+            inputSrc.setColumns(5);
+            inputDest.setColumns(5);
+            inputWeight.setColumns(5);
+            getSrcDestWeightPanel.add(new JLabel("Edge Source: "));
+            getSrcDestWeightPanel.add(inputSrc);
+            getSrcDestWeightPanel.add(new JLabel("Edge Destination: "));
+            getSrcDestWeightPanel.add(inputDest);
+            getSrcDestWeightPanel.add(new JLabel("New Edge Source: "));
+            getSrcDestWeightPanel.add(inputSrcNew);
+            getSrcDestWeightPanel.add(new JLabel("New Edge Destination: "));
+            getSrcDestWeightPanel.add(inputDestNew);
+            getSrcDestWeightPanel.add(editWeightCheckBox);
+            int res = JOptionPane.showConfirmDialog(null, getSrcDestWeightPanel,
+                    "Edit Edge", JOptionPane.OK_CANCEL_OPTION);
+            if (res== JOptionPane.OK_OPTION){
+                int src= Integer.parseInt(inputSrc.getText());
+                int dest= Integer.parseInt(inputDest.getText());
+                boolean keepWeight= editWeightCheckBox.isSelected();
+                double weight=1;
+                if(ga.getGraph().getNode(src)!=null && ga.getGraph().getNode(dest)!=null && ga.getGraph().getEdge(src,dest)!=null){
+                    weight=ga.getGraph().getEdge(src,dest).getWeight();
+                    if(!keepWeight){
+                        JPanel getNewWeight = new JPanel(new GridLayout(1,2));
+                        getNewWeight.add(new JLabel("New Weight :"));
+                        getNewWeight.add(inputWeight);
+                        int resW = JOptionPane.showConfirmDialog(null, getNewWeight,
+                                "New Weight", JOptionPane.OK_CANCEL_OPTION);
+                        if(!inputWeight.getText().isEmpty() && resW==JOptionPane.OK_OPTION) {
+                            weight = Double.parseDouble(inputWeight.getText());
+                        }else{
+                            JOptionPane.showMessageDialog(null,"Didnt give any value keeping old weight");
+                        }
+                    }
+                    ga.getGraph().removeEdge(src,dest);
+                    int newSrc= Integer.parseInt(inputSrcNew.getText());
+                    int newDest= Integer.parseInt(inputDestNew.getText());
+                    ga.getGraph().connect(newSrc,newDest,weight);
+                }else{
+                    JOptionPane.showMessageDialog(null,"Wrong Input");
+                }
+            }
+            getTopLevelAncestor().repaint();
+        }else if(e.getSource()==editVertex){
+            JPanel editVertexLocation= new JPanel(new GridLayout(2,2));
+            JTextField inputX = createFilteredTextField(true);
+            JTextField inputY = createFilteredTextField(true);
+            inputX.setColumns(5);
+            inputY.setColumns(5);
+            editVertexLocation.add(new JLabel("set X: "));
+            editVertexLocation.add(inputX);
+            editVertexLocation.add(new JLabel("set Y: "));
+            editVertexLocation.add(inputY);
+
+            JPanel chooseKey= new JPanel(new GridLayout(1,2));
+            JTextField inputKey= createFilteredTextField(false);
+            inputKey.setColumns(5);
+            chooseKey.add(new JLabel("Key: "));
+            chooseKey.add(inputKey);
+
+            int res= JOptionPane.showConfirmDialog(null, chooseKey,
+                    "Key", JOptionPane.OK_CANCEL_OPTION);
+            if (res==JOptionPane.OK_OPTION){
+                int key=Integer.parseInt(inputKey.getText());
+                if(ga.getGraph().getNode(key)!=null){
+                    NodeData temp=ga.getGraph().getNode(key);
+                    int resXY= JOptionPane.showConfirmDialog(null, editVertexLocation,
+                            "Choose Location", JOptionPane.OK_CANCEL_OPTION);
+                    if(resXY==JOptionPane.OK_OPTION){
+                        double x=Double.parseDouble(inputX.getText());
+                        double y=Double.parseDouble(inputY.getText());
+                        GeoLocationImpl xyNew= new GeoLocationImpl(x,y,0);
+                        temp.setLocation(xyNew);
+                    }
                 }else{
                     JOptionPane.showMessageDialog(null,"Wrong Input");
                 }
